ed992304a9bc4d8d83ab3c39435b68ea56bd3c4e
Ekta Brothers <ektabrothers314@gmail.com>
2020-07-12 13:13:03 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,6 +24,14 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy",policy=>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -38,7 +46,7 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
